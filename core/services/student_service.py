7dec56472d47c442d9807899c6a836d307fe61d5
T-H-10 <yh9336497@gmail.com>
2026-04-12 16:49:52 +0300

    add update and delete to studentservice

--- core/services/student_service.py
+++ core/services/student_service.py
@@ -17,3 +17,33 @@ class StudentService:
     @staticmethod
     def get_students(user):
         return Student.objects.for_user(user)
+    
+
+    @staticmethod
+    def get_student(user, student_id):
+        return Student.objects.for_user(user).filter(id=student_id).first()
+    
+    @staticmethod
+    def update_student(user, student_id, validated_data):
+        student = StudentService.get_student(user,student_id)
+
+        if not student:
+            return None
+        
+        for attr, value in validated_data.items():
+            setattr(student, attr, value)
+
+        student.save()
+        return student
+    
+
+    @staticmethod
+    def delete_student(user, student_id):
+        student = StudentService.get_student(user, student_id)
+
+        if not student:
+            return False
+        
+        student.delete()
+        return True
+
