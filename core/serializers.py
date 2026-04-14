2e4bceb3b48b51e17acafe9a027364389c0d10a8
T-H-10 <yh9336497@gmail.com>
2026-04-14 12:33:31 +0300

    add validations

--- core/serializers.py
+++ core/serializers.py
@@ -1,7 +1,28 @@
 from rest_framework import serializers
-from .models import Student
+from core.models import Student
+from core.validators import validate_phone, validate_id_number, validate_name
+
 
 class StudentSerializer(serializers.ModelSerializer):
+
+    def validate_full_name(self, value):
+        validate_name(value)
+        return value
+
+    def validate_id_number(self, value):
+        validate_id_number(value)
+        return value
+
+    def validate_mother_phone(self, value):
+        if value:
+            validate_phone(value)
+        return value
+
+    def validate_father_phone(self, value):
+        if value:
+            validate_phone(value)
+        return value
+
     class Meta:
         model = Student
         fields = "__all__"
