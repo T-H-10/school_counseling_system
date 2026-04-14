249193cfba263f4f71409b60b3fc7d298d83e8b3
T-H-10 <yh9336497@gmail.com>
2026-04-14 23:41:53 +0300

    add swagger

--- config/urls.py
+++ config/urls.py
@@ -15,13 +15,31 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import include, path
+from django.urls import include, path, re_path
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="School API",
+        default_version='v1',
+        description="API documentation",
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('core.urls')),
 
     path('api/token/', TokenObtainPairView.as_view()),
     path('api/token/refresh/', TokenRefreshView.as_view()),
+
+    re_path(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0)),
+    re_path(r'^redoc/$', schema_view.with_ui('redoc', cache_timeout=0)),
+
 ]
 
