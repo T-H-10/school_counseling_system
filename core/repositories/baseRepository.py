7aa452e04014e70c6c1dd96bcf609cc3eda914ef
T-H-10 <yh9336497@gmail.com>
2026-04-15 17:36:23 +0300

    add baseRepository

---
+++ core/repositories/baseRepository.py
@@ -0,0 +1,27 @@
+class BaseRepository:
+
+    model = None
+
+    @classmethod
+    def get_all(cls):
+        return cls.model.objects.all()
+
+    @classmethod
+    def get_by_id(cls, obj_id):
+        return cls.model.objects.filter(id=obj_id).first()
+
+    @classmethod
+    def create(cls, **data):
+        return cls.model.objects.create(**data)
+
+    @classmethod
+    def update(cls, instance, **data):
+        for attr, value in data.items():
+            setattr(instance, attr, value)
+
+        instance.save()
+        return instance
+
+    @classmethod
+    def delete(cls, instance):
+        instance.delete()
