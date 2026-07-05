f7a1cc8fe6a42f28372e68b7190a8a1b46437155
T-H-10 <t0534198344@gmail.com>
2026-07-05 17:08:39 +0300

    add wsgi to create admin user

--- config/wsgi.py
+++ config/wsgi.py
@@ -8,9 +8,28 @@ https://docs.djangoproject.com/en/6.0/howto/deployment/wsgi/
 """
 
 import os
-
 from django.core.wsgi import get_wsgi_application
+from django.contrib.auth import get_user_model
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
 
 application = get_wsgi_application()
+
+admin_username = os.environ.get("CREATE_ADMIN_USERNAME")
+admin_email = os.environ.get("CREATE_ADMIN_EMAIL")
+admin_password = os.environ.get("CREATE_ADMIN_PASSWORD")
+
+if admin_username and admin_email and admin_password:
+    try:
+        User = get_user_model()
+        if not User.objects.filter(username=admin_username).exists():
+            User.objects.create_superuser(
+                username=admin_username,
+                email=admin_email,
+                password=admin_password
+            )
+            print(f"--- Superuser '{admin_username}' created successfully from Env Vars! ---")
+        else:
+            print(f"--- Superuser '{admin_username}' already exists. ---")
+    except Exception as e:
+        print(f"--- Error creating superuser: {e} ---")
